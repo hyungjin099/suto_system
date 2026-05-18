16cba606ff79aa9214dbb4e7375a1c792aa1304a
hyungjin099 <hyungjin099@gmail.com>
2026-05-18 16:22:07 +0900

    start

---
+++ src/main/java/com/dream_comp/auto_system/AutoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dream_comp.auto_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream_comp/auto_system/AutoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream_comp.auto_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
